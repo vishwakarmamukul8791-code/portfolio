f256ca5e9d8a87a210f9cbf0113d37e42edfda4e
MUKUL <263430363+vishwakarmamukul8791-code@users.noreply.github.com>
2026-08-04 04:32:23 +0530

    Simplify footer

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+﻿import "./Footer.css"
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container footer__inner">
+        <span>Mukul</span>
+      </div>
+    </footer>
+  )
+}
